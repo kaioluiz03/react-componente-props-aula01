2806f3888689c109ef0c758ad3602708d451579d
Kaio <luizkaio9815@gmail.com>
2021-10-12 14:08:41 -0300

    Cria os arquivos da atividade Carta 11/10/21

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Carta } from './Components/Carta';
+import './Carta.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Carta remetente = 'Luiz' destino = 'Future'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Carta.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export const Carta = (props) => (
+    <div>
+        <h1>Olá, {props.remetente} - {props.destino}</h1>
+        <p>Estou escrevendo essa carta por causa da atividade que os alunos vão fazer, Okay. Não desiste, continue fazendo as atividades, treinando e praticando. O céu pe o limite!</p>
+    </div>
+)
